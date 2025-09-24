b954841a16ae44b634521ca1f3262adbaf6dce9e
Ryan Rittner <rrittner@purdue.edu>
2025-09-24 13:53:47 -0400

    Added main script to data_processing folder and added __init__ files

---
+++ src/data_processing/main.py
@@ -0,0 +1,15 @@
+import os
+from data_processing.embed_data import process_and_embed_data
+from preprocessing import process_policy_files
+
+
+def main():
+    # Run preprocessing and embedding scripts
+    data_folder = "/".join(os.path.abspath(__file__).split("/")[:-3]) + "/data/agora"
+    processed_data = process_policy_files(data_folder)
+    embeddings_path = process_and_embed_data(processed_data)
+    print("Vector embeddings stored at: " + embeddings_path)
+
+
+if __name__ == "__main__":
+    main()
