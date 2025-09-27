00710b3ee76495a02db656c8d2c387ae11452c5a
rrittner1 <rrittner@purdue.edu>
2025-09-27 18:15:34 +0000

    Got mistral 7B setup on lightning.ai

--- src/models/llm.py
+++ src/models/llm.py
@@ -1,12 +1,35 @@
 import os
+import torch
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.llms import HuggingFacePipeline
+from transformers import pipeline, AutoModelForCausalLM, MistralForCausalLM, AutoTokenizer
 
-def get_llm(provider: str = "gemini", params: dict = {}):
+def get_llm(provider: str = "hf", model_id: str = "mistralai/Mistral-7B-Instruct-v0.3", params: dict = {}):
     if provider == "gemini":
         return ChatGoogleGenerativeAI(
-            model="gemini-2.0-flash", 
+            model=model_id, 
             google_api_key=os.getenv("GEMINI_API_KEY"),
             **params
         )
-    else:
-        return None
+    elif provider == "hf":
+        if "mistral" in model_id:
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
+            model = MistralForCausalLM.from_pretrained(
+                model_id,
+                dtype="auto",
+                device_map="auto"
+            )
+        else:
+            model = AutoModelForCausalLM.from_pretrained(
+                model_id,
+                torch_dtype=torch.bfloat16,  
+                device_map="auto"             
+            )
+        pipe = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            max_new_tokens=1024,
+            temperature=0.2
+        )
+    return HuggingFacePipeline(pipeline=pipe)
