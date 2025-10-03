298f063d6f6d01b69cecafcafaf2c0335798ccb4
Saahil Mathur <saahilmathur@icloud.com>
2025-10-03 01:43:47 -0400

    Alter agent to work on UI

--- src/agent/main.py
+++ src/agent/main.py
@@ -1,8 +1,8 @@
 import os
-from src.agent.graph import build_agent_graph
+from src.agent.graph import build_terminal_agent_graph
 from src.utils import get_secrets
 
 if __name__ == "__main__":
     #os.environ["GEMINI_API_KEY"] = get_secrets()["GEMINI_API_KEY"]
-    agent = build_agent_graph()
+    agent = build_terminal_agent_graph()
     result = agent.invoke({})
